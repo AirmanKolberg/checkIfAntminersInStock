829b090e6e3a0d65dddde4ca676d5fa0e0bb9fc6
T.J. Kolberg <71570804+AirmanKolberg@users.noreply.github.com>
2021-02-08 22:02:56 -0700

    🤫  Sensitive Information

---
+++ example_secrets.py
@@ -0,0 +1,6 @@
+gmail_account = 'email@school.edu'
+gmail_password = 'password123'
+
+# This application assumes you're using a student E-Mail
+# If you are not, those steps can be skipped in program_functions.py
+username = 'myName'
