53890c1320bf5e66108c038bc80620bc9fb917a6
T.J. Kolberg <71570804+AirmanKolberg@users.noreply.github.com>
2021-02-08 22:11:24 -0700

    🔢 Important App Variables

--- robot_variables.py
+++ robot_variables.py
@@ -1,6 +1,3 @@
-from selenium import webdriver
-
-firefox = webdriver.Firefox()
 bitmain_url = 'https://shop.bitmain.com/product/detail?pid=00020201222165500548JAa69Gvu067A'
 gmail_url = 'https://accounts.google.com/signin/v2/identifier?service=mail&passive=true&rm=false&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&ss=1&scc=1&ltmpl=default&ltmplcache=2&emr=1&osid=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin'
 subject_line = 'ANTMINER IN STOCK!'
